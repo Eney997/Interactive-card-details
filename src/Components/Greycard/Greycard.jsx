6927ba7ba73810955bc5e650ecb543de4d2377b0
tornike dzidziguri <tornikedzidziguri2001@gmail.com>
2024-01-20 14:29:11 +0400

    changedCardInputs

--- src/Components/Greycard/Greycard.jsx
+++ src/Components/Greycard/Greycard.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
 import './Greycard.css'
-import { useState } from 'react'
 import Coloredbackground from '../forBack/Coloredbackground'
 
 
 const Greycard = (props) => {
    
-    const [value, setValue] = useState('');
-
-    const handleInputChange = (e) => {
-      let inputValue = e.target.value;
-      inputValue = inputValue.slice(0, 3);
-      setValue(inputValue);
-    };
+    
 
   return (
     <Coloredbackground>
    <div className='greyCardClass'>{props.children}
         <div className='blackDivsquear'></div>
-        <input 
-        type="number" 
-        value={value} 
-        onChange={handleInputChange} 
-        placeholder="000" 
-        className='numInputBox'/>
+        <div className='cvvDiv'>
+          <h1 className='cvvNums'>000</h1>
+        </div>
         <div className='greyLines'></div>
    </div>
    </Coloredbackground>
--- src/Components/Violetcard/Violetcard.jsx
+++ src/Components/Violetcard/Violetcard.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
 import Greycard from '../Greycard/Greycard'
 import './Violetcard.css'
-import { useState } from 'react'
 
 const Violetcard = () => {
-    const [valuee, setValuee] = useState('');
-
-    const handleInputChangee = (e) => {
-      let inputValuee = e.target.value;
-      inputValuee = inputValuee.slice(0, 16);
-      setValuee(inputValuee);
-    };
+    
 
   return (
     <Greycard>
         <div className='secondCard'>
             <div className='violetsCircle'></div>
-            <input 
-            id='input2'
-            type="number" 
-            value={valuee} 
-            onChange={handleInputChangee} 
-            placeholder="0000 0000 0000 0000" 
-            className='numInputBox2'/>
-            <input className='fottxtInput' maxLength={40} type='text' placeholder='Cardholder Name'></input>
-            <div className='expDay'>
-            <input className='monthIn' type="text" name="month" placeholder="MM" maxLength="2" size="2" />
-            <span className='forSpan'> /</span>
-            <input className='yearIn' type="text" name="year" placeholder="YY" maxLength="2" size="2" />
+            <h1 className='carldNumber'>0000 0000 0000 0000</h1>
+            <h2 className='cardHolder'>CARDHOLDER NAME</h2>
+            <div className='forSvv'>
+            <h3 className='YY'>00</h3>
+            <span className='slash'>/</span>
+            <h4 className='MM'>00</h4>
             </div>
         </div>
     </Greycard>
